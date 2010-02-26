3990f99cf01db322f1e685dc2a12dc406ab644fa
Derek Payton <derek@teahupoo.cukerinteractive.local>
2010-02-26 15:00:04 -0800

    Register with the correct admin site.

--- localedb/admin.py
+++ localedb/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-
-from simplecart.orders import SHOP_ADMIN_SITE
-
 from models import Locale, LocaleSiteDefault
 
-SHOP_ADMIN_SITE.register(Locale)
-SHOP_ADMIN_SITE.register(LocaleSiteDefault)
+admin.site.register(Locale)
+admin.site.register(LocaleSiteDefault)
