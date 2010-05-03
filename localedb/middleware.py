3e76d848394d71b3759c3905a71366edd8774677
Jason Kraus <jasonk@cukerinteractive.com>
2010-05-03 13:54:33 -0700

    we don't need no middleware

--- localedb/middleware.py
+++
@@ -1,8 +0,0 @@
-from localedb.models import Locale
-
-class LocaleDBMiddleware(object):
-    def process_request(self, request):
-        try:
-            request.locale = Locale.objects.get_site_locale()
-        except Locale.DoesNotExist:
-            request.locale = None
