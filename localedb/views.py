e6ff25af28301fc6c4f1a99a6681d2bba2ed35dc
Michael Bujol <michael@webcubecms.com>
2013-07-18 03:53:12 -0500

    remove simplejson (deprecated) in favor of json

--- localedb/views.py
+++ localedb/views.py
@@ -1,4 +1,4 @@
-from django.utils import simplejson
+import json
 from django.http import HttpResponse
 
 from models import Locale, get_locale
@@ -10,12 +10,12 @@ def ajax_currency_info(request, locale_name=None):
         try:
             locale = get_locale()
         except Locale.DoesNotExist:
-            return HttpResponse(simplejson.dumps(None), content_type="text/x-json")
+            return HttpResponse(json.dumps(None), content_type="text/x-json")
     ret = dict()
     for field in locale._meta.fields:
         key = field.name
         ret[key] = getattr(locale, key, None)
-    return HttpResponse(simplejson.dumps(ret), content_type="text/x-json")
+    return HttpResponse(json.dumps(ret), content_type="text/x-json")
     
 def ajax_currency(request, value, locale_name=None):
     if locale_name:
